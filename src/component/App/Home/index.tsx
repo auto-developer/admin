54be966142ee16074883e336eca03303db44bc77
dhc-zhuchuanpeng <chuanpeng1.zhu@dhc.com.cn>
2021-09-17 20:17:07 +0800

    init

--- src/component/App/Home/index.tsx
+++ src/component/App/Home/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import {Link} from "react-router-dom";
 
 function Dashboard() {
+    const {
+        VITE_CLIENT_ID,
+        VITE_OAUTH_SERVER,
+        VITE_REDIRECT_URI
+    } = import.meta.env
+
+    const oauthUri = `${VITE_OAUTH_SERVER}/authorize?client_id=${VITE_CLIENT_ID}&redirect_uri=${VITE_REDIRECT_URI}&response_type=code&scope=username&state=xyz`
+    const registerUri = `${VITE_OAUTH_SERVER}/sign-up`
     return <div>
         <h1>Home</h1>
         <ul>
-            <li><Link to="/sign-in">sign in</Link></li>
-            <li><Link to="/sign-up">sign up</Link></li>
+            <li><a href={oauthUri}>sign in</a></li>
+            <li><a href={registerUri}>sign up</a></li>
         </ul>
 
     </div>
--- src/component/App/index.tsx
+++ src/component/App/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import {Route, Switch} from "react-router";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
 import Home from "./Home";
 
 function App() {
 
     return <Switch>
         <Route path={'/'} strict={true} exact={true}><Home/></Route>
-        <Route path={'/sign-in'}><SignIn/></Route>
-        <Route path={'/sign-up'}><SignUp/></Route>
     </Switch>
 }
 
