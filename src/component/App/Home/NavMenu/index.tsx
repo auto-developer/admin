9605b39fae8d2a6fdb1338f7f9db6ff8dc18196a
dhc-zhuchuanpeng <chuanpeng1.zhu@dhc.com.cn>
2021-09-23 16:31:23 +0800

    client详情

--- src/component/App/Home/NavMenu/index.tsx
+++ src/component/App/Home/NavMenu/index.tsx
@@ -6,9 +6,9 @@ function NavMenu() {
 
     return <nav className={styles.nav}>
         <ul>
-            <li><NavLink to={`/`}>Dashboard</NavLink></li>
-            <li><NavLink to={`/users`}>User List</NavLink></li>
-            <li><NavLink to={`/clients`}>Client List</NavLink></li>
+            <li><NavLink to={`/`} strict={true} exact={true} activeClassName={styles.active}>Dashboard</NavLink></li>
+            <li><NavLink to={`/users`} activeClassName={styles.active}>User List</NavLink></li>
+            <li><NavLink to={`/clients`} activeClassName={styles.active}>Client List</NavLink></li>
         </ul>
     </nav>
 }
