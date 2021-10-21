31839ddf2f9299e1619c33e8c8c6e3a449df35e7
lossa <luomusha@gmail.com>
2021-10-21 15:00:06 +0800

    提取scope到env

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,3 +2,4 @@ export const CLIENT_ID = `${import.meta.env.VITE_CLIENT_ID}`
 export const CLIENT_SECRET = `${import.meta.env.VITE_CLIENT_SECRET}`
 export const OAUTH_SERVER = `${import.meta.env.VITE_OAUTH_SERVER}`
 export const REDIRECT_URI = `${import.meta.env.VITE_REDIRECT_URI}`
+export const SCOPE = `${import.meta.env.VITE_SCOPE}`
--- src/component/App/Login/index.tsx
+++ src/component/App/Login/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {CLIENT_ID, OAUTH_SERVER, REDIRECT_URI} from "../../../common/config";
+import {CLIENT_ID, OAUTH_SERVER, REDIRECT_URI, SCOPE} from "../../../common/config";
 
 function Login() {
 
-    const oauthUri = `${OAUTH_SERVER}/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=username&state=xyz`
+    const oauthUri = `${OAUTH_SERVER}/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}&state=xyz`
     const registerUri = `${OAUTH_SERVER}/sign-up`
     return <div>
         <h1>Login</h1>
